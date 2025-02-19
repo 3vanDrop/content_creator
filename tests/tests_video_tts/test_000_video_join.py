67dcad5fd6c95f2413b0754cea0f0dd8a444c22b
3vanDrop <evan.itf@gmail.com>
2025-02-18 20:34:14 -0700

    removed comment

--- tests/tests_video_tts/test_000_video_join.py
+++ tests/tests_video_tts/test_000_video_join.py
@@ -1,7 +1,7 @@
 from resources.media_join import video_audio_join, video_join
 from resources.buscar_clips import VideoDownloader
 
-#Esta es una prueba de video join
+
 def test_video_join():
     video_downloader = VideoDownloader(debug=True)  # Activar mensajes de depuración en la consola
     videos = video_downloader.query(name="food", count=5)

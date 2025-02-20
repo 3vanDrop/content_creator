05c0c1a8613bbfd3b8114731c5be3949e22f795b
Wency91 <erick_112098@hotmail.com>
2025-02-19 18:58:24 -0700

    fixes

--- resources/generar_voz_gtts.py
+++ resources/generar_voz_gtts.py
@@ -1,15 +1,15 @@
 from gtts import gTTS
 import os
 
-def generar_voz(texto_a_leer, idioma, output_path):
+def generar_voz(texto_a_leer, idioma, output_file):
     # Crear el objeto gTTS
     tts = gTTS(text=texto_a_leer, lang=idioma)
     
     # Guardar el archivo MP3 en la ruta especificada
-    tts.save(output_path)
+    tts.save(output_file)
     
     # Obtener la ruta absoluta del archivo generado
-    ruta_absoluta = os.path.abspath(output_path)
+    ruta_absoluta = os.path.abspath(output_file)
     
     return ruta_absoluta
 
--- tests/tests_functional/test_000_audio_tts.py
+++ tests/tests_functional/test_000_audio_tts.py
@@ -5,13 +5,13 @@ from tests.base_test import BaseTest
 class TestTTSAudio(BaseTest):
     def test_audio_gtts(self):
         audio_path = gtts_generar_voz("Hola este es un test case para generar audio.",
-                                idioma="es",
-                                output_path="test_audio.mp3")
+                                      idioma="es",
+                                      output_file="test_audio.mp3")
 
         assert self.file_exists(audio_path), f"Existe {audio_path}"
 
     def test_audio_edgetts(self):
         audio_path = edgetts_generar_voz("Hola este es un segundo test case para generar audio.",
-                                        output_path="test_audio2.mp3")
+                                         output_file="test_audio2.mp3")
 
         assert self.file_exists(audio_path), f"Existe {audio_path}"
